15e1285c4a303a7a7a393238bf13eb1dd6d0de89
RDAxRoadkill <wouter97@planet.nl>
2019-10-02 17:01:11 +0200

    Initial

---
+++ main.cpp
@@ -0,0 +1,33 @@
+/*
+ ****************************************************************************
+ * TITEL          main
+ * OMSCHRIJVING   Defineert het beginpunt van de applicatie
+ * BESTAND        main.cpp
+ * VERSIE		  1
+ * AUTEUR         G.A. Harkema, 20 MAART 2019
+ *
+ ****************************************************************************
+ */
+
+#include <iostream>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+int main()
+{
+	std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
